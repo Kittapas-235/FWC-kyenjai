e81bc54214972e071b04b580ea4e976c12227e29
Kittapas-235 <66070235@kmitl.ac.th>
2026-09-15 20:03:03 +0700

    Create balloon.js

---
+++ Cell03/ex01/balloon.js
@@ -0,0 +1,28 @@
+let balloon = document.getElementById("balloon");
+let size = 200;
+let colorIndex = 0;
+const colors = ["red", "green", "blue"];
+
+function updateBalloon() {
+    balloon.style.width = size + "px";
+    balloon.style.height = size + "px";
+    balloon.style.backgroundColor = colors[colorIndex];
+}
+
+balloon.addEventListener("click", () => {
+    size += 10;
+    colorIndex = (colorIndex + 1) % colors.length;
+    if (size > 420) {
+        size = 200;
+        colorIndex = 0;
+    }
+    updateBalloon();
+});
+
+balloon.addEventListener("mouseleave", () => {
+    if (size > 200) {
+        size -= 5;
+        colorIndex = (colorIndex - 1 + colors.length) % colors.length;
+        updateBalloon();
+    }
+});
